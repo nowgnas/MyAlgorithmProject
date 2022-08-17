03e54e424cd96ca302d4eba68c7171ab75219f33
nowgnas <marshmello.sw@gmail.com>
2022-08-17 20:29:36 +0900

    네트워크 성공~

--- src/algorithm/class0817/감시/Main.java
+++ src/algorithm/class0817/감시/Main.java
@@ -3,6 +3,7 @@ package algorithm.class0817.감시;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -14,8 +15,6 @@ public class Main {
     private static int n, m, black, answer;
     private static int[][] table, tempTable;
     private static List<int[]> cctv = new ArrayList<>();
-
-    private static int[][] cctv2;
     private static int[] cctvCase;
     private static int[][] direction = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
     private static int[] comb;
@@ -26,13 +25,13 @@ public class Main {
         System.out.println(answer);
     }
 
-    private static void combination(int idx) {
+    private static void combination(int idx) { // 조합을 해보자
 
-        if (idx == cctv.size()) {
+        if (idx == cctv.size()) { // cctv 개수만큼 조합이 완성 되먄
             tempTable = new int[n][m];
-            for (int i = 0; i < cctv.size(); i++) {
+            for (int i = 0; i < cctv.size(); i++) { // cctv 개수만큼 돌면서
                 int[] cctvData = cctv.get(i);
-                switch (cctvData[2]) {
+                switch (cctvData[2]) { // cctv 값 기준으로 0:i 1:j 2: val
                     case 1:
                         cctv_1(cctvData[0], cctvData[1], comb[i]);
                         break;
@@ -50,7 +49,7 @@ public class Main {
                         break;
                 }
             }
-            int count = black;
+            int count = black; // 사각지대 개수 count
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < m; j++) {
                     if (tempTable[i][j] == 1 && table[i][j] == 0) {
@@ -78,48 +77,49 @@ public class Main {
         for (int i = 0; i < n; i++) {
             table[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
-        black = 0;
+        black = 0; // 사각지대
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (table[i][j] <= 5 && table[i][j] >= 1) {
-                    cctv.add(new int[]{i, j, table[i][j]});
+                    cctv.add(new int[]{i, j, table[i][j]}); // 좌표 i, j, 테이블 값 넣기
                 }
-                if (table[i][j] == 0) {
+                if (table[i][j] == 0) { // 사각지대가 가능한 개수 세기
                     black += 1;
                 }
             }
         }
-        answer = black;
-        cctvCase = new int[cctv.size()];
-        comb = new int[cctv.size()];
+        answer = black; // 사각지대 예비 숫자
+        cctvCase = new int[cctv.size()]; // cctv가 동작할 수 있는 경우의 수
+        // 2, 5 인 경우는 다르고 나머지는 4개의 경우의 수가 있음
+        comb = new int[cctv.size()]; // cctv 개수만큼 배열 초기화
         for (int i = 0; i < cctv.size(); i++) {
             int[] ints = cctv.get(i);
-            if (ints[2] == 2) {
+            if (ints[2] == 2) { // 2번 cctv면 경우의 수 2
                 cctvCase[i] = 2;
-            } else if (ints[2] == 5) {
+            } else if (ints[2] == 5) { // 5번 cctv면 경우의 수 1
                 cctvCase[i] = 1;
-            } else {
+            } else { // 나머지 cctv는 경우의 수 4
                 cctvCase[i] = 4;
             }
         }
 
     }
 
-    private static void search(int y, int x, int idx) {
+    private static void search(int y, int x, int idx) { // 각 동작에서 직진하는 동작
         int ny = y + direction[idx][0];
         int nx = x + direction[idx][1];
-        while (0 <= ny && ny < n && 0 <= nx && nx < m && table[ny][nx] != 6) {
+        while (0 <= ny && ny < n && 0 <= nx && nx < m && table[ny][nx] != 6) { // map 밖으로 나가거나 6을 만나지 않는 경우 계속 진행
             tempTable[ny][nx] = 1;
             ny = ny + direction[idx][0];
             nx = nx + direction[idx][1];
         }
     }
 
-    private static void cctv_1(int y, int x, int idx) {
+    private static void cctv_1(int y, int x, int idx) { // 1번 cctv의 동작 : 한 방향으로 볼 수 있다
         search(y, x, idx);
     }
 
-    private static void cctv_2(int y, int x, int idx) {
+    private static void cctv_2(int y, int x, int idx) { // 2번 cctv의 동작 : 위 아래 방향 두가지의 경우의 수가 존재
         if (idx == 0) {
             search(y, x, 0);
             search(y, x, 2);
@@ -127,17 +127,16 @@ public class Main {
             search(y, x, 1);
             search(y, x, 3);
         }
-
     }
 
 
-    private static void cctv_3(int y, int x, int idx) {
+    private static void cctv_3(int y, int x, int idx) { // 3번 cctv
         search(y, x, idx % 4);
         search(y, x, (idx + 1) % 4);
     }
 
 
-    private static void cctv_4(int y, int x, int idx) {
+    private static void cctv_4(int y, int x, int idx) { // 4번 cctv 동작
         for (int i = 0; i < 4; i++) {
             if (i != idx) {
                 search(y, x, i);
@@ -145,7 +144,7 @@ public class Main {
         }
     }
 
-    private static void cctv_5(int y, int x, int idx) {
+    private static void cctv_5(int y, int x, int idx) { // 5번 cctv 동작
         for (int i = 0; i < 4; i++) {
             search(y, x, i);
         }
--- src/algorithm/study/week3/네트워크/Solution.java
+++ src/algorithm/study/week3/네트워크/Solution.java
@@ -3,27 +3,33 @@ package algorithm.study.week3.네트워크;
 
 public class Solution {
     static int n, cnt;
-    static int[] visit;
+    static boolean[] visit;
+    static boolean check;
 
     public static void main(String[] args) {
-        n = 3;
-        int[][] computers = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+
+//        int[][] computers = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+        int[][] computers = {{1, 1, 0}, {1, 1, 1}, {0, 1, 1}};
 
         cnt = 0;
-        visit = new int[n]; // 방문 여부 저장
-
-
-    }
-
-    private static void dfs(int dot, int[][] computers) {
-        if (visit[dot] == 1) return;
-        visit[dot] = 1;
+        n = 3;
+        visit = new boolean[n]; // 방문 여부 저장
         for (int i = 0; i < n; i++) {
-            // each item
-            if (dot == i) continue;
-            if (computers[dot][i] == 1) {
-                dfs(i, computers);
+            if (!visit[i]) {
+                cnt += 1;
+                dfs(computers, i); // 1이 아니면 탐색
+
             }
         }
+        System.out.println(cnt);
+    }
+
+    private static boolean dfs(int[][] computer, int idx) {
+        visit[idx] = true;
+        for (int i = 0; i < computer.length; i++) {
+            if (i != idx && computer[idx][i] == 1 && !visit[i])
+                check = dfs(computer, i);
+        }
+        return check;
     }
 }
